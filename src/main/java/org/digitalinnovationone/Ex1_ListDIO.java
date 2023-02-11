44643c284b607f57904c9d3ebb2036a111f08b1f
Thiago Hashimoto <thiago.hashimoto94@gmail.com>
2023-02-11 17:55:38 -0300

    Primeiros exemplos de Ordenação de List da DIO e propostas dos exercícios

---
+++ src/main/java/org/digitalinnovationone/Ex1_ListDIO.java
@@ -0,0 +1,13 @@
+package org.digitalinnovationone;
+
+/*
+
+Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista.
+
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro e etc).
+
+ */
+
+public class Ex1_ListDIO {
+}
