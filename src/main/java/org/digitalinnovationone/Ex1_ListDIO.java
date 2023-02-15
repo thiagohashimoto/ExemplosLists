33f903c882574a9c85bf9fd7c19363a0cd04c809
Thiago Hashimoto <thiago.hashimoto94@gmail.com>
2023-02-14 21:29:44 -0300

    Primeira resolução dos exercícios propostos de Lists da DIO

--- src/main/java/org/digitalinnovationone/Ex1_ListDIO.java
+++ src/main/java/org/digitalinnovationone/Ex1_ListDIO.java
@@ -1,5 +1,10 @@
 package org.digitalinnovationone;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 
 Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista.
@@ -8,6 +13,41 @@ Após isto, calcule a média semestral das temperaturas e mostre todas as temper
 e em que mês elas ocorreram (mostrar o mês por extenso: 1 - Janeiro, 2 - Fevereiro e etc).
 
  */
-
 public class Ex1_ListDIO {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double soma = 0.0;
+
+        List<Double> temperaturaSemestral = new ArrayList<>();
+
+        for (int i = 0 ; i < 6 ; i++){
+            System.out.print("Insira a temperatura do mês " + (i+1) + ": ");
+            double temp = scan.nextDouble();
+            temperaturaSemestral.add(temp);
+            soma += temp;
+        }
+        double mediaTemperatura = soma/temperaturaSemestral.size();
+
+        System.out.println("Média semestral: " + mediaTemperatura);
+
+        for (Double temp : temperaturaSemestral) {
+            if (temp > mediaTemperatura) {
+                int indexpos = temperaturaSemestral.indexOf(temp);
+                switch (indexpos) {
+                    case 0 -> System.out.println((indexpos + 1) + " - Janeiro: " + temp + "ºC");
+                    case 1 -> System.out.println((indexpos + 1) + " - Fevereiro: " + temp + "ºC");
+                    case 2 -> System.out.println((indexpos + 1) + " - Março: " + temp + "ºC");
+                    case 3 -> System.out.println((indexpos + 1) + " - Abril: " + temp + "ºC");
+                    case 4 -> System.out.println((indexpos + 1) + " - Maio: " + temp + "ºC");
+                    case 5 -> System.out.println((indexpos + 1) + " - Junho: " + temp + "ºC");
+                }
+            }
+        }
+
+
+
+    }
 }
+
--- src/main/java/org/digitalinnovationone/Ex2_ListDIO.java
+++ src/main/java/org/digitalinnovationone/Ex2_ListDIO.java
@@ -14,5 +14,54 @@ entre 3 e 4 como "Cúmplice" e 5 como "Assassina". Caso contrário, ela será cl
 
  */
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Ex2_ListDIO {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<String> questionario = new ArrayList<>();
+
+
+        System.out.print("Telefonou para a vítima? ");
+        String resposta = scan.next();
+        questionario.add(resposta);
+
+        System.out.print("Esteve no local do crime? ");
+        resposta = scan.next();
+        questionario.add(resposta);
+
+        System.out.print("Mora perto da vítima? ");
+        resposta = scan.next();
+        questionario.add(resposta);
+
+        System.out.print("Devia para a vítima? ");
+        resposta = scan.next();
+        questionario.add(resposta);
+
+        System.out.print("Já trabalhou com a vítima? ");
+        resposta = scan.next();
+        questionario.add(resposta);
+
+        int countAnswers = 0; //Contador de respostas
+
+        for (String respostas : questionario ) {
+            if(respostas.toLowerCase().contains("s")){
+                countAnswers++;
+            }
+        }
+
+        System.out.println("----------");
+        System.out.println("A pessoa é: ");
+
+        switch (countAnswers) {
+            case 0 -> System.out.println("--\tInocente\t--");
+            case 1 -> System.out.println("--\tInocente\t--");
+            case 2 -> System.out.println("--\tSuspeita\t--");
+            case 3, 4 -> System.out.println("--\tCúmplice\t--");
+            case 5 -> System.out.println("--\tAssassino\t--");
+        }
+    }
 }
