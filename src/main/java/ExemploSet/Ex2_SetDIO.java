ed8f74121c7140168a35ca71530b0d74311bfb54
Thiago Hashimoto <thiago.hashimoto94@gmail.com>
2023-03-02 22:12:24 -0300

    Primeira resolução dos exercício proposto 02 de Sets da DIO

--- src/main/java/ExemploSet/Ex2_SetDIO.java
+++ src/main/java/ExemploSet/Ex2_SetDIO.java
@@ -9,5 +9,96 @@ d) Ano de criação e nome
 e) Nome, ano de criação e IDE
  */
 
+import java.util.*;
+
 public class Ex2_SetDIO {
+    public static void main(String[] args) {
+        Set<LinguagemFavorita> minhasLinguagensFavoritas = new HashSet<>();
+            minhasLinguagensFavoritas.add(new LinguagemFavorita("Python", 1991, "Pycharm"));
+            minhasLinguagensFavoritas.add(new LinguagemFavorita("JavaScript", 1995, "IntelliJ"));
+            minhasLinguagensFavoritas.add(new LinguagemFavorita("Java", 1991, "Visual Studio Code"));
+
+        System.out.println("--\tOrdem de Inserção\t--");
+
+        Set<LinguagemFavorita> minhasLinguagensFavoritas1 = new LinkedHashSet<>(
+            Arrays.asList(
+                    new LinguagemFavorita("Python", 1991, "Pycharm"),
+                    new LinguagemFavorita("JavaScript", 1995, "IntelliJ"),
+                    new LinguagemFavorita("Java", 1991, "Visual Studio Code")
+                    )
+        );
+        for (LinguagemFavorita linguagem : minhasLinguagensFavoritas)
+            System.out.println(linguagem);
+
+        System.out.println("--\tOrdem Natural\t--");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas2 = new TreeSet<>(new Comparator<LinguagemFavorita>() {
+            @Override
+            public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+                return CharSequence.compare(o1.getNome(),o2.getNome());
+            }
+        });
+            minhasLinguagensFavoritas2.addAll(minhasLinguagensFavoritas);
+            for (LinguagemFavorita linguagem1 : minhasLinguagensFavoritas2)
+                System.out.println(linguagem1);
+
+        System.out.println("--\tOrdem por IDE\t--");
+        Set<LinguagemFavorita> minhasLinguagensFavoritas3 = new TreeSet<LinguagemFavorita>(new ComparatorIde());
+        minhasLinguagensFavoritas3.addAll(minhasLinguagensFavoritas);
+        for (LinguagemFavorita linguagem2 : minhasLinguagensFavoritas3)
+            System.out.println(linguagem2);
+
+        System.out.println("--\tOrdem por Ano de Criação e Nome\t--");
+
+    }
 }
+
+class LinguagemFavorita{
+    public String nome;
+    public Integer anoDeCriacao;
+    public String ide;
+
+    public LinguagemFavorita(String nome, Integer anoDeCriacao, String ide) {
+        this.nome = nome;
+        this.anoDeCriacao = anoDeCriacao;
+        this.ide = ide;
+    }
+
+    @Override
+    public String toString() {
+        return "LinguagemFavorita{" +
+                "nome='" + nome + '\'' +
+                ", anoDeCriacao=" + anoDeCriacao +
+                ", IDE='" + ide + '\'' +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public Integer getAnoDeCriacao() {
+        return anoDeCriacao;
+    }
+    public String getIde() {
+        return ide;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinguagemFavorita that = (LinguagemFavorita) o;
+        return Objects.equals(nome, that.nome) && Objects.equals(anoDeCriacao, that.anoDeCriacao) && Objects.equals(ide, that.ide);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, anoDeCriacao, ide);
+    }
+}
+
+class ComparatorIde implements Comparator<LinguagemFavorita>{
+    @Override
+    public int compare(LinguagemFavorita f1, LinguagemFavorita f2) {
+        return f1.ide.compareToIgnoreCase(f2.ide);
+    }
+}
+
